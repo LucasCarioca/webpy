c38dd5f8eb70caef7c1c02c56d7cecf5e8b94d8f
Lucas Desouza <lucasfds2777@gmail.com>
2019-11-06 23:32:18 -0600

    initial upload

---
+++ main.py
@@ -0,0 +1,47 @@
+import web
+import json
+
+
+class Index:
+    def GET(self):
+        response = {
+            'message': 'Simple python service',
+            'version': '0.0.1'
+        }
+        web.header('Content-Type', 'application/json')
+        return json.dumps(dict(response))
+
+
+class PersonalHelloController:
+    def GET(self, name):
+        response = {
+            'message': message(name)
+        }
+        web.header('Content-Type', 'application/json')
+        return json.dumps(dict(response))
+
+
+class HelloController:
+    def GET(self):
+        response = {
+            'message': message('world')
+        }
+        web.header('Content-Type', 'application/json')
+        return json.dumps(dict(response))
+
+
+def message(name):
+    return 'Hello {}!'.format(name)
+
+
+urls = (
+    '/', Index,
+    '\\/hello\\/', HelloController,
+    '\\/hello', HelloController,
+    '\\/hello\\/([^\\/]+)\\/', PersonalHelloController,
+    '\\/hello\\/([^\\/]+)', PersonalHelloController,
+)
+
+if __name__ == '__main__':
+    app = web.application(urls, globals())
+    app.run()
